a6e91106939c038c1cf77c32b884a0a91f359dfe
Steven Nagle <SteveNagle@Outlook.com>
2021-11-18 16:22:26 -0500

    Added user/menu interaction

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelector('.nav-item');
+const menuButton = document.querySelector('.menu-btn');
+const menuLeftPhoto = document.querySelector('.menu-Left-Photo');
+
+let showMenu = false;
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    showMenu = true;
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuButton.classList.add('close');
+    menuLeftPhoto.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+  }
+  else {
+    showMenu = false;
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuButton.classList.remove('close');
+    menuLeftPhoto.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+  }
+}
